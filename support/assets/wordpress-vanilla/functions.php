9c8f77edeee647ed41119a692e6db604d1c7dbb2
ethus3h <kolubat@gmail.com>
2018-02-06 16:21:30 -0500

    Update Ember tree

--- support/assets/wordpress-vanilla/functions.php
+++ support/assets/wordpress-vanilla/functions.php
@@ -33,9 +33,6 @@ function twentyseventeen_setup() {
 	 */
 	load_theme_textdomain( 'twentyseventeen' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -45,32 +42,10 @@ function twentyseventeen_setup() {
 	add_theme_support( 'title-tag' );
 
 	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
-
-	add_image_size( 'twentyseventeen-featured-image', 2000, 1200, true );
-
-	add_image_size( 'twentyseventeen-thumbnail-avatar', 100, 100, true );
-
-	// Set the default content width.
-	$GLOBALS['content_width'] = 525;
-
-	// This theme uses wp_nav_menu() in two locations.
-	register_nav_menus( array(
-		'top'    => __( 'Top Menu', 'twentyseventeen' ),
-		'social' => __( 'Social Links Menu', 'twentyseventeen' ),
-	) );
-
-	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
 	add_theme_support( 'html5', array(
-		'comment-form',
-		'comment-list',
 		'gallery',
 		'caption',
 	) );
