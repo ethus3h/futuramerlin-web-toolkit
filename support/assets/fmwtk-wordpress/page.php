316ed811621ad005bf970dda9d16ba3b35aad3a0
ethus3h <kolubat@gmail.com>
2018-02-06 16:10:59 -0500

    Update Ember tree

--- support/assets/fmwtk-wordpress/page.php
+++ support/assets/fmwtk-wordpress/page.php
@@ -10,15 +10,32 @@
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage FMWTK_Wordpress
- * @since 0.0.1
- * @version 0.0.1
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
  */
 
 get_header(); ?>
-<?php
-while ( have_posts() ) : the_post();
-	get_template_part( 'template-parts/page/content', 'page' );
-endwhile; // End of the loop.
-?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
 <?php get_footer();
