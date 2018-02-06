d2d768d1806782131a5b51420339a4c8f7007e06
ethus3h <kolubat@gmail.com>
2018-02-06 16:12:47 -0500

    Update Ember tree

--- support/assets/fmwtk-wordpress/header.php
+++ support/assets/fmwtk-wordpress/header.php
@@ -23,3 +23,6 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<?php if ( has_nav_menu( 'top' ) ) : ?>
+			<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+	<?php endif; ?>
