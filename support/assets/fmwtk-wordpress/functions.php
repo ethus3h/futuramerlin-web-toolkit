2693816c15052c0d993f8de81abcf5cf873ee66c
ethus3h <kolubat@gmail.com>
2018-02-08 13:02:59 -0500

    Update Ember tree

--- support/assets/fmwtk-wordpress/functions.php
+++ support/assets/fmwtk-wordpress/functions.php
@@ -2,11 +2,8 @@
 function my_theme_enqueue_styles() {
     $parent_style = 'twentyseventeen-style';
 
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+    wp_enqueue_style( 'm', '/../../../m.css' );
+    wp_enqueue_style( 'child-style', '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
