a6b29782cf16f7b2ce2841e19bf63ee9ffb81a9e
ethus3h <kolubat@gmail.com>
2018-02-08 12:57:10 -0500

    Update Ember tree

---
+++ support/assets/twentyseventeen/fmwtk-wordpress/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage FMWTK_Wordpress
+ * @since 0.0.1
+ * @version 0.0.1
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php if ( has_nav_menu( 'top' ) ) : ?>
+			<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+	<?php endif; ?>
