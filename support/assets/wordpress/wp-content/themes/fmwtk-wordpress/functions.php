85521ec6d92abad672bbe11e0703d2e17dca8329
ethus3h <kolubat@gmail.com>
2018-02-09 11:22:44 -0500

    Update Ember tree

--- support/assets/wordpress/wp-content/themes/fmwtk-wordpress/functions.php
+++ support/assets/wordpress/wp-content/themes/fmwtk-wordpress/functions.php
@@ -11,3 +11,7 @@ function remove_admin_login_header() {
     remove_action('wp_head', '_admin_bar_bump_cb');
 }
 add_action('get_header', 'remove_admin_login_header');
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
