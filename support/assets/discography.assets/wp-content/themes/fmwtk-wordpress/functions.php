54459a5e8a97e4182aaa1985a8c62c231b193396
ethus3h <kolubat@gmail.com>
2018-04-18 10:02:25 -0400

    Update Ember tree

--- support/assets/discography.assets/wp-content/themes/fmwtk-wordpress/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function my_theme_enqueue_styles() {
-    $parent_style = 'twentyseventeen-style';
-
-    wp_enqueue_style( 'm', '/../../../m.css' );
-    wp_enqueue_style( 'child-style', '/style.css' );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
