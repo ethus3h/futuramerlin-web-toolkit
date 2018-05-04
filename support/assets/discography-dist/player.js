926a5000b734d5cec4ed302095a78b98b388ef82
ethus3h <kolubat@gmail.com>
2018-05-04 19:29:03 -0400

    Update Git repository using egup-single

--- support/assets/discography-dist/player.js
+++ support/assets/discography-dist/player.js
@@ -27,7 +27,7 @@ function formatSecondsAsTime(secs, format) {
     return min + ':' + sec;
 }
 
-let currentTrack = 1;
+let currentTrack;
 
 let audioTag = document.createElement('audio');
 
@@ -255,14 +255,7 @@ function pauseTrackFromTrackButton(trackClickedElement) {
 function reachedEndOfTrack(eventParameter) {
     console.log('Reached end of track ' + currentTrack);
     currentTrackElement = document.getElementsByClassName('currentTrack')[0];
-    let tempTrack = 0;
-    for (let i = 1; i < trackRows.length; i++) {
-        /* skip first row: it is header */
-        if (trackRows[i].getElementsByTagName('td')[1].getElementsByTagName('button')[0] === currentTrackElement) {
-            tempTrack = i;
-        }
-    }
-    nextTrack = tempTrack + 1;
+    nextTrack = currentTrack + 1;
     numberOfTracks = trackRows.length - 1;
     if (nextTrack > numberOfTracks) {
         nextTrack = 1;
