6e1875e553ab4cd8b65b5d816e5cf3f998b6e5c3
ethus3h <kolubat@gmail.com>
2018-05-04 20:10:49 -0400

    Update Git repository using egup-single

--- support/assets/discography-dist/player.js
+++ support/assets/discography-dist/player.js
@@ -210,12 +210,12 @@ function playLabelSetLoading() {
 function playLabelSetPaused() {
     clearTrackStatuses();
     let trackPlayButton = trackRows[currentTrack].getElementsByTagName('td')[1].getElementsByTagName('button')[0];
-    trackPlayButton.innerHTML = "⏸";
+    trackPlayButton.innerHTML = "▶";
     trackPlayButton.className = 'playButton playing currentTrack';
     trackPlayButton.onclick = function() {
         playTrackFromTrackButton(this);
     };
-    audioMainPlayButton.innerHTML = "⏸";
+    audioMainPlayButton.innerHTML = "▶";
 }
 
 function playTrack(trackNumber) {
